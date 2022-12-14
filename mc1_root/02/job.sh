3b3c598f9d757cd93806de46fecabe709df8527e
seanlo <seanlo@mit.edu>
2022-12-14 10:17:46 +0800

    remove Gurobi dependency

--- mc1_root/02/job.sh
+++ mc1_root/02/job.sh
@@ -6,9 +6,6 @@ source /etc/profile
 # Loading modules
 module purge
 module load julia/1.7.3
-module load gurobi/gurobi-951
-
-GUROBI_LICENSE_FILE="/slo/gurobi.lic"
 
 echo "My SLURM_ARRAY_TASK_ID: " $LLSUB_RANK
 echo "Number of Tasks: " $LLSUB_SIZE
